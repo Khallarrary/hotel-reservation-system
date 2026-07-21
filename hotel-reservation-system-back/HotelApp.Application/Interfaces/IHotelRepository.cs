8344e905a76aefbb54f3ea3b7d15bc3b8363ea4d
José Aparecido Fernandes Junior <joseap.fer@hotmail.com>
2026-07-21 02:32:46 -0300

    inicia estrutura de hotéis para isolamento de ambientes
    cria entidade Hotel com dados básicos e status
    adiciona validações de nome, documento e fuso horário
    normaliza o documento antes do armazenamento
    adiciona métodos para ativar e desativar hotéis
    inicia contrato do repository de hotéis

---
+++ hotel-reservation-system-back/HotelApp.Application/Interfaces/IHotelRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HotelApp.Domain;
+
+namespace HotelApp.Application.Interfaces
+{
+    internal interface IHotelRepository
+    {
+        Task AdicionarAsync(Hotel hotel);
+        Task<Hotel?> ObterPorIdAsync(int id);
+        Task<Hotel?> ObterPorDocumentoAsync(string documento);
+        Task<List<Hotel>> ListarAsync();
+        Task AtualizarAsync(Hotel hotel);
+    }
+}
---
+++ hotel-reservation-system-back/HotelApp.Domain/Hotel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelApp.Domain
+{
+    public class Hotel
+    {
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Documento { get; private set; }
+        public bool Ativo { get; private set; }
+        public string FusoHorario{ get; private set; }
+
+        private Hotel() { }
+        public Hotel(string nome, string documento, string fusoHorario) 
+        {
+            var normalizado = new string(documento.Where(char.IsDigit).ToArray());
+
+            if (string.IsNullOrWhiteSpace(nome)) 
+            {
+                throw new ArgumentException("Nome é obrigatório");
+            }
+
+            if (string.IsNullOrWhiteSpace(normalizado))
+            {
+                throw new ArgumentException("Documento é obrigatório");
+            }
+
+
+            if (string.IsNullOrWhiteSpace(fusoHorario))
+            {
+                throw new ArgumentException("Fuso horario é obrigatório");
+            }
+
+            
+
+            Nome = nome;
+            Documento = documento;
+            FusoHorario = fusoHorario;
+            Ativo = true;
+        }
+
+        public void Ativar()
+        {
+            Ativo = true;
+        }
+
+        public void Desativar()
+        {
+            Ativo = false;
+        }
+    }
+}
